0def517fe03f5e12603541a520a4b786e4059dec
Amine Benneji <aminebneji@gmail.com>
2024-09-05 17:36:55 +0200

    workflow

--- service/greetings.js
+++ service/greetings.js
@@ -1,9 +1,9 @@
 const greet = name => {
-  return `Hello, ${name}!`
+  return `Hello, ${name}`
 }
 
 const bye = name => {
-  return `Goodbye, ${name}!`
+  return `Goodbye, ${name}`
 }
 
 module.exports = { greet, bye }
